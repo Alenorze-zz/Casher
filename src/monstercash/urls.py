2c5cef6ff4873b6b2b73f46f8068bb0320bf7835
Алексей Белов <Alenorze@gmail.com>
2018-01-14 16:24:17 +0200

    Swagger DOCS

--- src/monstercash/urls.py
+++ src/monstercash/urls.py
@@ -1,21 +1,12 @@
-"""
-Definition of urls for monstercash.
-"""
-
+from django.contrib import admin
+from django.views.generic import TemplateView
 from django.conf.urls import include, url
+from rest_framework_swagger.views import get_swagger_view
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+schema_view = get_swagger_view(title='Pastebin API')
+
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', monstercash.views.home, name='home'),
-    # url(r'^monstercash/', include('monstercash.monstercash.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/$', schema_view),
+    url(r'^admin/', include(admin.site.urls)),
 ]
