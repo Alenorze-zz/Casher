03b9f501c5049bb651b40ece8427b570318028f3
Алексей Белов <Alenorze@gmail.com>
2018-01-14 16:56:32 +0200

    Django Rest Framework

---
+++ src/monstercash/restconf/pagination.py
@@ -0,0 +1,10 @@
+from rest_framework import pagination
+
+
+
+class CFEAPIPagination(pagination.LimitOffsetPagination): #PageNumberPagination):
+    #page_size   =  20
+    default_limit   = 10
+    max_limit       = 20
+    #limit_query_param = 'lim' 
+    
